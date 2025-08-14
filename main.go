ba8f89aaea5530da4e45deeb9802832572e3cce9
Hayden Rieder <73128440+Hayden50@users.noreply.github.com>
2025-08-13 21:17:34 -0400

    fetching from sleeper

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	logrus.WithFields(logrus.Fields{
-		"animal": "walrus",
-	}).Info("A walrus appears")
+
+	res, httpErr := http.Get("https://api.sleeper.app/v1/league/1182033902772338688")
+	if httpErr != nil {
+		logrus.Error("error fetching content from sleeper app")
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		logrus.Error("error reading response body: ", err)
+		return
+	}
+
+	fmt.Printf("HTTP Status: %s\n", res.Status)
+	fmt.Printf("Response Body:\n%s\n", body)
 }
